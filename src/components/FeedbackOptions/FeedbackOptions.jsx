182efb9703f224276612eb4432dc1d2d25da8530
Olena Voitsekhivska <89697984+OlenkaVoitsekhivska@users.noreply.github.com>
2022-05-08 20:19:42 +0300

    added styles

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,17 +1,19 @@
-function FeedbackOptions ({onLeaveFeedback}){
-    return (
-      <div>   
-        <button onClick={onLeaveFeedback} id ='good'>
-          Good
-        </button>
-        <button onClick={onLeaveFeedback} id = 'neutral'>
-          Neutral
-        </button>
-        <button onClick={onLeaveFeedback} id = 'bad'>
-          Bad
-        </button>
-        </div>
-    )
+import style from './FeedbackOptions.module.css';
+
+function FeedbackOptions({ onLeaveFeedback }) {
+  return (
+    <div className={style.box}>
+      <button onClick={onLeaveFeedback} id="good" className={style.good}>
+        Good
+      </button>
+      <button onClick={onLeaveFeedback} id="neutral" className={style.neutral}>
+        Neutral
+      </button>
+      <button onClick={onLeaveFeedback} id="bad" className={style.bad}>
+        Bad
+      </button>
+    </div>
+  );
 }
 
 export default FeedbackOptions;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,16 +1,12 @@
-function Statistics({good, neutral, bad, total, positivePercentage}) {
+function Statistics({ good, neutral, bad, total, positivePercentage }) {
   return (
-    <>
-  
-      <h2>Statistics</h2>
-      <ul>
-        <li>Good: {good}</li>
-        <li>Neutral: {neutral}</li>
-        <li>Bad: {bad}</li>
-        <li>Total: {total}</li>
-        <li>Positive feedback: {good === 0 ? 0 : positivePercentage}%</li>
-      </ul>
-    </>
+    <ul>
+      <li>Good: {good}</li>
+      <li>Neutral: {neutral}</li>
+      <li>Bad: {bad}</li>
+      <li>Total: {total}</li>
+      <li>Positive feedback: {good === 0 ? 0 : positivePercentage}%</li>
+    </ul>
   );
 }
 
